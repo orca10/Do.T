4f9785363860711d781da586550733b7d881165d
orca10 <syawlaekawa@gmail.com>
2023-02-18 22:39:22 +0900

    [FIX]modify SubHeader.js

--- components/SubLayout.js
+++ components/SubLayout.js
@@ -1,8 +1,19 @@
 import SubHeader from './SubHeader';
+import { useRouter } from 'next/router'
 
 const SubLayout = ({ children }) => {
-
   var HeaderTitle = ""
+  const router = useRouter()
+
+  if (router.pathname.includes('scholarship')) {
+    HeaderTitle = "장학금"
+  }
+  if (router.pathname.includes('contest')) {
+    HeaderTitle = "공모전"
+  }
+  if (router.pathname.includes('activity')) {
+    HeaderTitle = "대외활동"
+  }
 
   return (
     <div className='flex mx-auto justify-center '>
